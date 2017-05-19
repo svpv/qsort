64e364402fd408697fae233764b1b0e6e7cba2c7
Alexey Tourbin <alexey.tourbin@gmail.com>
2017-05-19 22:13:54 +0300

    qsort.h: first take, implemented partitioning
    This incorporates some of my findings from 2013.
    
    Producing a general-purpose sort routine can be hard, because the cost
    of comparison is unknown.  Quicksort works best when comparisons are
    cheap (e.g. comparing two integers takes roughly one instruction, while
    swapping two integers takes roughly three instructions).  When
    comparisons are expensive (e.g. strcmp), minimizing the number of
    comparisons becomes very important; hence merge sort might work best
    in this case.
    
    Quicksort with median-of-3 and insertion sort cut-off makes about
    1.2n log_2 n - 3n comparisons on the average.  Merge sort makes
    n log_2 n comparisons in the worst case (and close to that in the
    average case), roughly 10% fewer.  To some extent, this justifies the
    use of merge sort in glibc.  Glibc's merge sort is somewhat suboptimal,
    though: it has extra branches in the inner loop (instead of choosing the
    right loop), and it calls memcpy to swap elements (instead of inlining
    and/or predefining a bunch of copyN functions).  But optimizing glibc's
    qsort is another story.
    
    The upshot is that both rdtsc cycles AND the number of comparisons
    must be taken into account.
    
    For example, consider this partitioning loop:
    
        while (1) {
    	do q_i++; while (Q_LESS(q_i, q_l));
    	do q_j--; while (Q_LESS(q_l, q_j));
    	if (q_i >= q_j) break;
    	Q_SWAP(q_i, q_j);
        }
    
    The loop is very tight, but it makes one extra useless iteration when
    q_i + 1 == q_j.  There's then the temptation to amend the loop like this:
    
    	Q_SWAP(q_i, q_j);
    +	if (q_i + 1 == q_j) {
    +	    q_i++, q_j--;
    +	    break;
    +	}
    
    It is interesting to estimate both theoretically and practically what
    kind of gains can such an optimization bring.
    
    Let's assume that the optimization can save 1 comparison per partition.
    Perfect but naiive Quicksort make 1 + 2 + 4 + ... + n/2, or about n,
    partitions.  Thus the optimization can save n / (n log_2 n), or about
    10%, of comparisons (these are very rough estimates indeed).
    A non-naiive Quicksort, on the other hand, the one which switches to
    insertion sort after the size of partition becomes 16 or less,
    makes much fewer partitions.  Specifically, for each partition of
    size 16, it skips 14 subsequent partitions:
    
    	   8       8
    	 4   4   4   4
    	2 2 2 2 2 2 2 2
    
    So, with the insertion-sort cut-off, the reduction in comparisons should
    be below 1%.  Indeed, test runs show that with n=1024, about 0.3% to 0.4%
    fewer comparisons are made.  (With these test runs, though, there's
    another problem: enough of them must be done with srand(getpid()),
    due to the dependency on input data inherent to Quicksort.)  And so
    I conclude that it is probably not worth to amend the loop.

---
+++ qsort.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2013, 2017 Alexey Tourbin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/* Sort 3 elements. */
+#define Q_SORT3(a1, a2, a3)		\
+do {					\
+    if (Q_LESS(a2, a1)) {		\
+	if (Q_LESS(a3, a2))		\
+	    Q_SWAP(a1, a3);		\
+	else {				\
+	    Q_SWAP(a1, a2);		\
+	    if (Q_LESS(a3, a2))		\
+		Q_SWAP(a2, a3);		\
+	}				\
+    }					\
+    else if (Q_LESS(a3, a2)) {		\
+	Q_SWAP(a2, a3);			\
+	if (Q_LESS(a2, a1))		\
+	    Q_SWAP(a1, a2);		\
+    }					\
+} while (0)
+
+/* Partition [q_l,q_r] around a pivot.  After partitioning,
+ * [q_l,q_j] are the elements that are less than or equal to the pivot,
+ * while [q_i,q_r] are the elements greater than or equal to the pivot. */
+#define Q_PARTITION(q_l, q_r, q_i, q_j)					\
+do {									\
+    /* The middle element, not to be confused with the median. */	\
+    Q_UINT q_m = q_l + ((q_r - q_l) >> 1);				\
+    /* Sort 3 items so that the smallest is placed in the middle,	\
+     * the median at the beginning, and the largest at the end. */	\
+    Q_SORT3(q_m, q_l, q_r);						\
+    /* The smallest element should not remain in the middle,		\
+     * place it right after the median. */				\
+    Q_SWAP(q_m, q_l + 1);						\
+    /* Partition [q_l+2, q_r-1] around the median which is in q_l.	\
+     * q_i and q_j are initially off by one, they get decremented	\
+     * in the do-while loops. */					\
+    q_i = q_l + 1; q_j = q_r;						\
+    while (1) {								\
+	do q_i++; while (Q_LESS(q_i, q_l));				\
+	do q_j--; while (Q_LESS(q_l, q_j));				\
+	if (q_i >= q_j) break; /* Sedgewick says "until j < i" */	\
+	Q_SWAP(q_i, q_j);						\
+    }									\
+    /* Compensate for the i==j case. */					\
+    q_i = q_j + 1;							\
+    /* Put the median to its final place. */				\
+    Q_SWAP(q_l, q_j);							\
+    /* The median is not part of the left subfile. */			\
+    q_j--;								\
+} while (0)
+
+/* ex:set ts=8 sts=4 sw=4 noet: */
